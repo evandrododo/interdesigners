fd032e52defa91cef6ede6a5fbc7b2be55b0a150
Mateus Batista <mateusbatistasantos@gmail.com>
2015-09-03 20:50:24 -0300

    Simpósio: àrea de contato com a comissão científica e os respectivos currículos lattes

--- resources/views/simposio/contato.blade.php
+++ resources/views/simposio/contato.blade.php
@@ -3,10 +3,80 @@
 Qualquer dúvida que possa surgir por favor nos contacte via email <a href="mailto:interdesigners.unesp@gmail.com">interdesigners.unesp@gmail.com</a>  ou pela página do  <b><a target="_blank" href='https://www.facebook.com/interdesigners'>facebook</a></b> .
 <br>
 <h4>
-    <img  src="/img/simposio/comissao_cientifica.png" alt="Datas Importantes" id="comissao-cientifica">
+    <img  src="/img/simposio/comissao_cientifica.png" alt="Comissão Cient´ifica" id="comissao-cientifica">
 </h4>
-Em breve.
-
-<br><Br>
-
+<br>
+<ul>
+<li><a href="#" data-toggle="popover" title="Áreas de Correção"  data-trigger="hover" data-content="
+	<li>Design Gráfico;</li>
+	<li>Comunicação Visual;</li>
+	<li>Fundamentos teóricos; </li>
+	<li>História do Design;</li>
+	<li>Design Téxtil e de Vestuário.</li>">Beatriz Pereira de Andrade</a><a href="http://lattes.cnpq.br/6505808233970471"><img src="/img/icon-lattes.png" class="img-lattes" /></a></li>
+<br>
+<li><a href="#" data-toggle="popover" title="Áreas de Correção"  data-trigger="hover" data-content="
+	<li>Comunicação Visual;</li>
+	<li>Design Gráfico;</li>
+	<li>Design Inclusivo; </li>
+	<li>Design Informacional;</li>
+	<li>Fundamentos Teóricos;</li>
+	<li>História do Design (Gráfico).</li>">Cassia Leticia Carrara Domiciano</a><a href="http://lattes.cnpq.br/1534045638750216"><img src="/img/icon-lattes.png" class="img-lattes" /></a></li>
+<br>
+<li><a href="#" data-toggle="popover" title="Áreas de Correção"  data-trigger="hover" data-content="
+	<li>Design de Produto; </li>
+	<li>Gestão de Design;</li>
+	<li>Materiais e Processos em Design.</li>">Osmar Vicente Rodrigues</a><a href="http://lattes.cnpq.br/7037225941389554"><img src="/img/icon-lattes.png" class="img-lattes" /></a></li>
+<br>
+<li><a href="#" data-toggle="popover" title="Áreas de Correção"  data-trigger="hover" data-content="
+	<li>História do Design;</li>
+	<li>Design de Interiores;</li>
+	<li>Design de Produto;</li>
+	<li>Design Urbano;</li>
+	<li>Fundamentos Teóricos.</li>">Paula da Cruz Landim</a><a href="http://lattes.cnpq.br/4943484003365191"><img src="/img/icon-lattes.png" class="img-lattes" /></a></li>
+<br>
+<li><a href="#" data-toggle="popover" title="Áreas de Correção"  data-trigger="hover" data-content="
+	<li>Eco Design (Design Sustentável);</li>
+	<li>Design de produto;</li>
+	<li>Materiais e Processos em Design.</li>">Tomas Queiroz Ferreira Barat</a><a href="http://lattes.cnpq.br/7865768257571169"><img src="/img/icon-lattes.png" class="img-lattes" /></a></li>
+<br>
+<li><a href="#" data-toggle="popover" title="Áreas de Correção"  data-trigger="hover" data-content="
+	<li>Design Gráfico; </li>
+	<li>Design Informacional; </li>
+	<li>Materiais e Processos em Design;</li>
+	<li>Comunicação Visual.</li>">Milton Koji Nakata</a><a href="http://lattes.cnpq.br/0203116221000967"><img src="/img/icon-lattes.png" class="img-lattes" /></a></li>
+<br>
+<li><a href="#" data-toggle="popover" title="Áreas de Correção"  data-trigger="hover" data-content="
+	<li>Design de Produto;</li>
+	<li>Ergonomia e Acessibilidade; </li>
+	<li>Fundamentos Teóricos.</li>">Ana Cristina Maurício Ferreira</a><a href="http://lattes.cnpq.br/7226501479696435"><img src="/img/icon-lattes.png" class="img-lattes" /></a></li>
+<br>
+<li><a href="#" data-toggle="popover" title="Áreas de Correção"  data-trigger="hover" data-content="
+	<li>Design de Produto; </li>
+	<li>Design Inclusivo;</li>
+	<li>Design Urbano.</li>">Claudemilson dos Santos</a><a href="http://lattes.cnpq.br/1443427423117005"><img src="/img/icon-lattes.png" class="img-lattes" /></a></li>
+<br>
+<li><a href="#" data-toggle="popover" title="Áreas de Correção"  data-trigger="hover" data-content="
+	<li>Design Informacional; </li>
+	<li>Comunicação Visual;</li>
+	<li>Semiótica em Design.</li>">Fabiane Rodrigues Fernandes</a><a href="http://lattes.cnpq.br/0490329444455023"><img src="/img/icon-lattes.png" class="img-lattes" /></a></li>
+<br>
+<li><a href="#" data-toggle="popover" title="Áreas de Correção"  data-trigger="hover" data-content="
+	<li>Design de Produto; </li>
+	<li>Ergonomia e Acessibilidade;</li>
+	<li>Eco Design;</li>
+	<li>Materiais e Processos em Design.</li>">Franciane da Silva Falcão</a><a href="http://lattes.cnpq.br/0416128517251375"><img src="/img/icon-lattes.png" class="img-lattes" /></a></li>
+<br>
+<li><a href="#" data-toggle="popover" title="Áreas de Correção"  data-trigger="hover" data-content="
+	<li>Design de Produto; </li>
+	<li>Design Inclusivo;</li>
+	<li>Ergonomia e Acessibilidade.</li>">Gabriel Henrique Cruz Bonfim</a><a href="http://lattes.cnpq.br/7000927324833901"><img src="/img/icon-lattes.png" class="img-lattes" /></a></li>
+</ul>
 Esperamos vocês para construir este evento e trocar conhecimento!
+<script>
+$(document).ready(function(){
+    $('[data-toggle="popover"]').popover({html: true});
+    $('[data-toggle="popover"]').on('click', function(e){
+    	e.preventDefault();
+    });
+});
+</script>
