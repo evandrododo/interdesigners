fea41b53e2bdd9e9ecd9acf6498a8c06e73b7555
Evandro Carreira <evandro.carreira@gmail.com>
2015-09-10 01:06:25 -0300

    Javascript sendo executado sempre que é chamada a pagina de submissao

--- resources/assets/js/ajaxload.js
+++ resources/assets/js/ajaxload.js
@@ -114,6 +114,35 @@ function scriptsUrl(url) {
         	// }
         	nextAuthor.removeClass('input-hidden');
         	nextAuthor.show();
+
+          //token do laravel para ajax
+          $.ajaxSetup({
+              headers: { 'X-CSRF-TOKEN': $('input[name="_token"]').attr('value') }
+          });
+
+          /** Funcões para Upload */
+          $('.inscricao > form').submit(function (ev) {
+              ev.preventDefault();
+              var frm = $(this),
+                  dataForm = new FormData(this),
+                  callbackFunction = frm.data('callback');
+              $.ajax({
+                  type: frm.attr('method'),
+                  url: frm.attr('action'),
+                  data: dataForm,
+                  contentType: false, //file
+                  processData: false,  //file
+                  success: function (data) {
+
+                      $('.modal-backdrop ').html(data);
+
+                      // if(callbackFunction) {
+                      //     eval(callbackFunction+"()");
+                      // }
+                  }
+              });
+          });
+
         });
     }else{
         $('#anima_simposio').addClass('transparente');
--- resources/assets/js/components/simposio/form.js
+++ resources/assets/js/components/simposio/form.js
@@ -1,31 +1,4 @@
 $(document).ready( function(){
-  //token do laravel para ajax
-  $.ajaxSetup({
-      headers: { 'X-CSRF-TOKEN': $('input[name="_token"]').attr('value') }
-  });
 
-  /** Funcões para Upload */
-  $('.inscricao > form').submit(function (ev) {
-      ev.preventDefault();
-      var frm = $(this),
-          dataForm = new FormData(this),
-          callbackFunction = frm.data('callback');
-      $.ajax({
-          type: frm.attr('method'),
-          url: frm.attr('action'),
-          data: dataForm,
-          contentType: false, //file
-          processData: false,  //file
-          success: function (data) {
 
-              $('.modal-backdrop ').html(data);
-
-              // if(callbackFunction) {
-              //     eval(callbackFunction+"()");
-              // }
-          }
-      });
-  });
-
-  
 });
